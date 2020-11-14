01ba68200f8214cf5cbe31267a58203b696a06a4
Ali <zhangjc1999@gmail.com>
2020-11-14 14:03:34 +0800

    define basic type checking

---
+++ project2/semantic.cpp
@@ -0,0 +1,12 @@
+#include "semantic.hpp"
+
+bool typecheck(Type *left, Type*right, bool weak) {
+    if (*left==*EMPTYTYPE || *right==*EMPTYTYPE)
+        return true;
+    else if (weak && typeid(*left) == typeid(Primitive_Type) && typeid(*right) == typeid(Primitive_Type))
+        return true;
+    else
+        return (*left == *right);
+}
+Type *EMPTYTYPE = new Type();
+Variable_Type *EMPTYVAR = new Variable_Type("$", EMPTYTYPE);
